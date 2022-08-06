52c40e0c4edc3e10c569e34d6595d47f4caa17bf
Bryan Lancien <bryanlancien@yahoo.fr>
2022-08-06 22:40:13 +0100

    add missing files

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import Button from './components/Button.vue';
+
+export default {
+  components: {
+    Button
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <Button />
+  </div>
+</template>
+
+<style lang="scss">
+h1 {
+  color: green;
+
+  &:hover {
+    color: greenyellow;
+  }
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++" class="btn btn-primary">You clicked me {{ count }} times.</button>
+</template>
